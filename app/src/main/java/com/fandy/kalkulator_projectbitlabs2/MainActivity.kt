7d59d01b63b07022b4bce231ba4e733c6f84bd32
fandy2000 <61608860+fandy2000@users.noreply.github.com>
2020-04-26 16:40:25 +0700

    Project Pendamping Project2 Bitlabs

---
+++ app/src/main/java/com/fandy/kalkulator_projectbitlabs2/MainActivity.kt
@@ -0,0 +1,63 @@
+package com.fandy.kalkulator_projectbitlabs2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun tambah (view: View){
+        val angka1 = input1.text.toString().toInt();
+        val angka2 = input2.text.toString().toInt();
+
+        val res = angka1 + angka2
+        result.text = res.toString()
+
+    }
+
+
+    fun kurang (view: View){
+
+        val angka1 = input1.text.toString().toInt();
+        val angka2 = input2.text.toString().toInt();
+
+        val res = angka1 - angka2
+        result.text = res.toString()
+
+
+
+    }
+
+
+    fun bagi (view: View){
+
+        val angka1 = input1.text.toString().toInt();
+        val angka2 = input2.text.toString().toInt();
+
+        val res = angka1 / angka2
+        result.text = res.toString()
+
+
+
+    }
+
+
+    fun kali (view: View){
+
+        val angka1 = input1.text.toString().toInt();
+        val angka2 = input2.text.toString().toInt();
+
+        val res = angka1 * angka2
+        result.text = res.toString()
+
+
+
+    }
+
+}
